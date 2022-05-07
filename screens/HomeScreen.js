b7b3f384cca75919164fbd45ec109956ab8d7aa6
Yolanda Theodorakis <theodorakisyolanda@gmail.com>
2022-05-07 14:17:34 +0300

    Location demo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Linking, Platform} from 'react-native';
 
 import Colors from '../constants/Colors';
 
 export default function HomeScreen() {
+    const sendMessage = () => {
+    const url = (Platform.OS === 'android')
+    ? 'sms:+358403503325?body=your message'
+    : 'sms:919999999999'
+    Linking.canOpenURL(url).then(supported => {
+      if (!supported) {
+        console.log('Unsupported url: ' + url)
+      } else {
+        return Linking.openURL(url)
+      }
+    }).catch(err => console.error('An error occurred', err))}
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>SAFE</Text>
+            <TouchableOpacity onPress={sendMessage}>
+                <Text>SEND</Text>
+            </TouchableOpacity>
         </View>
     );
 }
